9bf0b3e527bbef4c693acfbae019ca1fec89e314
LeofkJ <leofkjourdan@gmail.com>
2019-01-15 18:51:32 -0500

    Add files via upload
    Added the third rotor's List of tuples

--- Enigma_cypher.py
+++ Enigma_cypher.py
@@ -1,9 +1,15 @@
 REFLECTOR = [("Y","A"),("R","B"),("U","C"),("H","D"),("Q","E"),("S","F"),("L","G"),("P","I"),("X","J"),("N","K"),("O","M"),("Z","T"),("W","V")]
-ROTOR_I= [()]
+
+ROTOR_III = [("A","B"),("B","D"),("C","F"),("D","H"),("E","J"),("F","L"),("G","C"),("H","P"),("I","R"),("J","T"),("K","X"),("L","V"),("M","Z"),("N","N"),("O","Y"),("P","E"),("Q","I"),("R","W"),("S","G"),("T","A"),("U","K"),("V","M"),("W","U"),("X","S"),("Y","Q"),("Z","O")]
 
 class Rotor:
     def __init__(self):
         # TODO
 class Reflector:
+    """ This class represents the reflector of the device.
+    ===ATTRIBUTES===
+    REFLECTOR: List of Tuples that represents the reflector, placed at the end of the rotors
+
+    """
     def __init__(self):
         # TODO
