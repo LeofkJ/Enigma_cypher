11e71194b7fe1763716dc7a7766b0e1032eed29e
LeofkJ <leofkjourdan@gmail.com>
2019-01-15 18:39:44 -0500

    Add files via upload
    Added the List of Tuples of the Reflector part of the machine

--- Enigma_cypher.py
+++ Enigma_cypher.py
@@ -1,3 +1,6 @@
+REFLECTOR = [("Y","A"),("R","B"),("U","C"),("H","D"),("Q","E"),("S","F"),("L","G"),("P","I"),("X","J"),("N","K"),("O","M"),("Z","T"),("W","V")]
+ROTOR_I= [()]
+
 class Rotor:
     def __init__(self):
         # TODO
